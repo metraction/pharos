b3cb70282ad97ac3504f6098711e8cf380789152
Frank <frank@enpace.ch>
2025-06-21 11:19:00 +0200

    add swagger at /api/swagger

--- cmd/http.go
+++ cmd/http.go
@@ -48,6 +48,34 @@ These submissions are then published to a Redis stream for further processing by
 		controllers.NewimageController(&api, currentConfig).WithPublisher(client).AddRoutes()
 		controllers.NewPharosScanTaskController(&api, currentConfig).WithPublisher(client).AddRoutes()
 
+		// I love swagger
+
+		router.HandleFunc("/api/swagger", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/html")
+			w.Write([]byte(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="SwaggerUI" />
+  <title>SwaggerUI</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
+</head>
+<body>
+<div id="swagger-ui"></div>
+<script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
+<script>
+  window.onload = () => {
+    window.ui = SwaggerUIBundle({
+      url: '/api/openapi.json',
+      dom_id: '#swagger-ui',
+    });
+  };
+</script>
+</body>
+</html>`))
+		})
+
 		router.HandleFunc("/submit/image", routing.SubmitImageHandler(client, currentConfig))
 		serverAddr := fmt.Sprintf(":%d", httpPort)
 		logger.Info().Str("address", serverAddr).Msg("Starting HTTP server")
