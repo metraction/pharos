40a8f8dd6ce9fb7136bf6d8e1a70b00f35bc3403
Frank <132054700+frwa-enpace@users.noreply.github.com>
2025-07-11 10:48:43 +0200

    Ph 94/metrics (#96)
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics

--- internal/controllers/metricscontroller.go
+++ internal/controllers/metricscontroller.go
@@ -4,11 +4,13 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
+	_ "github.com/dustinkirkland/golang-petname"
 	"github.com/metraction/pharos/internal/integrations/redis"
 	"github.com/metraction/pharos/internal/logging"
 	"github.com/metraction/pharos/pkg/model"
@@ -19,56 +21,90 @@ import (
 
 type MetricsController struct {
 	Path              string
-	Vulnerabilities   prometheus.GaugeVec
-	Contexts          prometheus.GaugeVec
 	Api               *huma.API
 	AsyncPublisher    *redis.RedisGtrsClient[model.PharosScanTask2, model.PharosScanResult]
 	PriorityPublisher *redis.RedisGtrsClient[model.PharosScanTask2, model.PharosScanResult]
 	Config            *model.Config
 	Logger            *zerolog.Logger
+	contextLabels     map[string]string // Used to collect labels for contexts metric
 }
 
 func NewMetricsController(api *huma.API, config *model.Config) *MetricsController {
-	var vulnerabilities = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "pharos_vulnerabilities",
-		Help: "Vulnerabilites for images",
-	}, []string{"image", "digest", "imageid", "platform", "severity"})
-
-	var contexts = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "pharos_test",
-		Help: "Vulnerabilites for images",
-	}, []string{})
-
-	prometheus.MustRegister(vulnerabilities)
 	mc := &MetricsController{
-		Path:            "/metrics",
-		Api:             api,
-		Config:          config,
-		Logger:          logging.NewLogger("info", "component", "MetricsController"),
-		Vulnerabilities: *vulnerabilities,
-		Contexts:        *contexts,
+		Path:   "/metrics",
+		Api:    api,
+		Config: config,
+		Logger: logging.NewLogger("info", "component", "MetricsController"),
 	}
 	return mc
 }
 
 func (mc *MetricsController) AddRoutes() {
 	{
-		op, handler := mc.Metrics()
+		op, handler := mc.VulnerbilityMetrics()
+		huma.Register(*mc.Api, op, handler)
+	}
+	{
+		op, handler := mc.ContextMetrics()
+		huma.Register(*mc.Api, op, handler)
+	}
+	{
+		op, handler := mc.DefaultMetrics()
 		huma.Register(*mc.Api, op, handler)
 	}
 }
 
-// Gets metrics for pharos controller, vulnerabilities, and images. We are registering an operation with a handler, and use writer and request from a middleware.
+// returns a registry for serving context metrics, may return nil if no context labels are set yet.
+func (mc *MetricsController) NewContextRegistry() (*prometheus.Registry, *prometheus.GaugeVec, error) {
+	if mc.contextLabels == nil {
+		return nil, nil, errors.New("No context labels set for metrics")
+	}
+	registry := prometheus.NewRegistry()
+	desc := prometheus.GaugeOpts{
+		Name: "pharos_contexts",
+		Help: "Contexts for images",
+	}
+	keys := []string{}
+	values := []string{}
+	for i, v := range mc.contextLabels {
+		keys = append(keys, i)
+		values = append(values, v)
+	}
+	contexts := prometheus.NewGaugeVec(desc, keys)
+
+	err := registry.Register(contexts)
+	if err != nil {
+		return nil, nil, err
+
+	}
+	return registry, contexts, nil
+}
+
+func (mc *MetricsController) NewVulnerabilityRegistry() (*prometheus.Registry, *prometheus.GaugeVec, error) {
+	registry := prometheus.NewRegistry()
+	desc := prometheus.GaugeOpts{
+		Name: "pharos_vulnerabilities",
+		Help: "Vulnerabilities for images",
+	}
+	vulnerabilities := prometheus.NewGaugeVec(desc, []string{"image", "digest", "imageid", "platform", "severity"})
+	err := registry.Register(vulnerabilities)
+	if err != nil {
+		return nil, nil, err
+
+	}
+	return registry, vulnerabilities, nil
+}
+
+// Gets Vulnerability metrics for pharos controller, vulnerabilities, and images. We are registering an operation with a handler, and use writer and request from a middleware.
 // A bit of a hack, but now we have a common way to document things.
 
-func (mc *MetricsController) Metrics() (huma.Operation, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error)) {
-	h := promhttp.Handler()
+func (mc *MetricsController) VulnerbilityMetrics() (huma.Operation, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error)) {
 	return huma.Operation{
-			OperationID: "GetMetrics",
+			OperationID: "GetVulnerbilityMetrics",
 			Method:      "GET",
-			Path:        mc.Path,
-			Summary:     "Gets metrics for pharos controller, vulnerabilities, and images",
-			Description: "Gets metrics for pharos controller, vulnerabilities, and images",
+			Path:        mc.Path + "/vulnerabilities",
+			Summary:     "Gets Vulnerbility metrics",
+			Description: "Gets Vulnerbility metrics",
 			Tags:        []string{"Metrics"},
 			Responses: map[string]*huma.Response{
 				"200": {
@@ -83,14 +119,18 @@ func (mc *MetricsController) Metrics() (huma.Operation, func(ctx context.Context
 			},
 		}, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error) {
 			timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
-				mc.Logger.Info().Float64("duration_seconds", v).Msg("Metrics handler duration")
+				mc.Logger.Info().Str("Handler", "VulnerbilityMetrcs").Float64("duration_seconds", v).Msg("Metrics handler duration")
 			}))
 			defer timer.ObserveDuration()
+			registry, vulnerabilites, err := mc.NewVulnerabilityRegistry()
+			if err != nil {
+				return nil, huma.Error500InternalServerError("Failed to create vulnerbility registry: " + err.Error())
+			}
+			h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 			databaseContext, err := getDatabaseContext(ctx)
 			if err != nil {
 				return nil, huma.Error500InternalServerError("Database context not found in request context")
 			}
-			mc.Vulnerabilities.Reset()
 			mc.Logger.Info().Msg("Serving metrics")
 			writer := ctx.Value("writer").(http.ResponseWriter)
 			request := ctx.Value("request").(*http.Request)
@@ -98,16 +138,12 @@ func (mc *MetricsController) Metrics() (huma.Operation, func(ctx context.Context
 			if err := databaseContext.DB.Find(&images).Error; err != nil {
 				return nil, huma.Error500InternalServerError("Failed to retrieve Docker images: " + err.Error())
 			}
-			// This is a two step process:
-			// 1. We first iterate over all images and collect the labels we need for and set the vulnerabilities metric.
-			// 2. We then create a GaugeVec with the labels we collected and set the values for the contexts metric.
-			var labels = map[string]string{
+			mc.contextLabels = map[string]string{
 				"image":    "",
 				"digest":   "",
 				"imageid":  "",
 				"platform": "",
 			}
-			// step 1: Collect labels and set vulnerabilities metric
 			for _, image := range images {
 				var fullImage model.PharosImageMeta
 				if err := databaseContext.DB.Preload("Findings").Preload("ContextRoots.Contexts").First(&fullImage, "image_id = ?", image.ImageId).Error; err != nil {
@@ -117,35 +153,63 @@ func (mc *MetricsController) Metrics() (huma.Operation, func(ctx context.Context
 					mc.Logger.Debug().Str("imageId", image.ImageId).Any("summary", summary).Msg("Found image in database")
 
 					for level, count := range summary.Severities {
-						mc.Vulnerabilities.WithLabelValues(fullImage.ImageSpec, fullImage.IndexDigest, fullImage.ImageId, fullImage.ArchOS+"/"+fullImage.ArchName, level).Set(float64(count))
+						vulnerabilites.WithLabelValues(fullImage.ImageSpec, fullImage.IndexDigest, fullImage.ImageId, fullImage.ArchOS+"/"+fullImage.ArchName, level).Set(float64(count))
 					}
 					for _, contextRoot := range fullImage.ContextRoots {
 						for _, context := range contextRoot.Contexts {
 							for label := range context.Data {
-								labels[label] = ""
+								mc.contextLabels[label] = ""
 							}
 						}
 					}
 
 				}
 			}
-			// Step 2: Create GaugeVec for contexts metric and fill it with data
-			// First we have to know what labels we have to expect, so we can create the GaugeVec with the correct labels.
-			// Now that we have all the labels, we can create the contexts metric
-			desc := prometheus.GaugeOpts{
-				Name: "pharos_contexts",
-				Help: "Contexts for images",
+			h.ServeHTTP(writer, request)
+			return nil, nil
+		}
+}
+
+func (mc *MetricsController) ContextMetrics() (huma.Operation, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error)) {
+
+	return huma.Operation{
+			OperationID: "GetContextMetrics",
+			Method:      "GET",
+			Path:        mc.Path + "/contexts",
+			Summary:     "Gets context metrics",
+			Description: "Gets context metrics",
+			Tags:        []string{"Metrics"},
+			Responses: map[string]*huma.Response{
+				"200": {
+					Content: map[string]*huma.MediaType{
+						"text/plain": {},
+					},
+					Description: "Metrics",
+				},
+				"500": {
+					Description: "Internal server error",
+				},
+			},
+		}, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error) {
+			timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+				mc.Logger.Info().Str("Handler", "ContextMetrics").Float64("duration_seconds", v).Msg("Metrics handler duration")
+			}))
+			defer timer.ObserveDuration()
+			registry, contexts, err := mc.NewContextRegistry()
+			if err != nil {
+				return nil, huma.Error500InternalServerError("Failed to create context registry: " + err.Error())
 			}
-			keys := []string{}
-			values := []string{}
-			for i, v := range labels {
-				keys = append(keys, i)
-				values = append(values, v)
+			h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+			writer := ctx.Value("writer").(http.ResponseWriter)
+			request := ctx.Value("request").(*http.Request)
+			databaseContext, err := getDatabaseContext(ctx)
+			if err != nil {
+				return nil, huma.Error500InternalServerError("Database context not found in request context")
 			}
-			contexts := prometheus.NewGaugeVec(desc, keys)
-			prometheus.Unregister(contexts) // Unregister if it already exists to avoid duplicate registration
-			prometheus.MustRegister(contexts)
-			// now we have to iterate again, and write the contexts
+			var images []model.PharosImageMeta
+			if err := databaseContext.DB.Find(&images).Error; err != nil {
+				return nil, huma.Error500InternalServerError("Failed to retrieve Docker images: " + err.Error())
+			}
 			for _, image := range images {
 				var fullImage model.PharosImageMeta
 				if err := databaseContext.DB.Preload("Findings").Preload("ContextRoots.Contexts").First(&fullImage, "image_id = ?", image.ImageId).Error; err != nil {
@@ -154,22 +218,57 @@ func (mc *MetricsController) Metrics() (huma.Operation, func(ctx context.Context
 					for _, contextRoot := range fullImage.ContextRoots {
 						// we must reset the labels first
 						// Empty values will be ignored by Prometheus, so we can just set them to empty strings.
-						for label := range labels {
-							labels[label] = ""
+						for label := range mc.contextLabels {
+							mc.contextLabels[label] = ""
 						}
-						labels["image"] = fullImage.ImageSpec
-						labels["digest"] = fullImage.IndexDigest
-						labels["imageid"] = fullImage.ImageId
-						labels["platform"] = fullImage.ArchOS + "/" + fullImage.ArchName
+						mc.contextLabels["image"] = fullImage.ImageSpec
+						mc.contextLabels["digest"] = fullImage.IndexDigest
+						mc.contextLabels["imageid"] = fullImage.ImageId
+						mc.contextLabels["platform"] = fullImage.ArchOS + "/" + fullImage.ArchName
 						for _, context := range contextRoot.Contexts {
 							for label, value := range context.Data {
-								labels[label] = fmt.Sprintf("%v", value)
+								mc.contextLabels[label] = fmt.Sprintf("%v", value)
 							}
 						}
-						contexts.With(labels).Set(1)
+						contexts.With(mc.contextLabels).Set(1)
 					}
 				}
 			}
+
+			h.ServeHTTP(writer, request)
+			return nil, nil
+		}
+}
+
+func (mc *MetricsController) DefaultMetrics() (huma.Operation, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error)) {
+
+	return huma.Operation{
+			OperationID: "GetMetrics",
+			Method:      "GET",
+			Path:        mc.Path,
+			Summary:     "Gets default metrics",
+			Description: "Gets default metrics",
+			Tags:        []string{"Metrics"},
+			Responses: map[string]*huma.Response{
+				"200": {
+					Content: map[string]*huma.MediaType{
+						"text/plain": {},
+					},
+					Description: "Metrics",
+				},
+				"500": {
+					Description: "Internal server error",
+				},
+			},
+		}, func(ctx context.Context, input *struct{}) (*struct{ Body string }, error) {
+			timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+				mc.Logger.Info().Str("Handler", "ContextMetrics").Float64("duration_seconds", v).Msg("Metrics handler duration")
+			}))
+			defer timer.ObserveDuration()
+			writer := ctx.Value("writer").(http.ResponseWriter)
+			request := ctx.Value("request").(*http.Request)
+			h := promhttp.Handler() // default handler for Prometheus metrics
+
 			h.ServeHTTP(writer, request)
 			return nil, nil
 		}
