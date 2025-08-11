dde1267b421d112eeed4de6239959a57e233fd1c
Frank <frank@enpace.ch>
2025-08-11 18:41:44 +0200

    add alerts

--- pkg/model/alerting_test.go
+++ pkg/model/alerting_test.go
@@ -39,47 +39,4 @@ func TestGetPrometheusAlert(t *testing.T) {
 	}
 }
 
-func TestGetPrometheusAlert_Nil(t *testing.T) {
-	var alert *Alert
-	if alert.GetPrometheusAlert() != nil {
-		t.Error("Expected nil PrometheusAlert for nil Alert")
-	}
-}
-
-func TestGetFingerprint(t *testing.T) {
-	alert := &Alert{
-		Labels: []AlertLabel{
-			{Name: "b", Value: "2"},
-			{Name: "a", Value: "1"},
-		},
-	}
-	fp1 := alert.GetFingerprint()
-	// Changing order should not change fingerprint
-	alert.Labels = []AlertLabel{
-		{Name: "a", Value: "1"},
-		{Name: "b", Value: "2"},
-	}
-	fp2 := alert.GetFingerprint()
-	if fp1 != fp2 {
-		t.Errorf("Expected fingerprint to be order-independent, got %s and %s", fp1, fp2)
-	}
-}
-
-func TestGetFingerprint_EmptyLabels(t *testing.T) {
-	alert := &Alert{}
-	fp := alert.GetFingerprint()
-	if fp != "none" {
-		t.Errorf("Expected fingerprint 'none' for empty labels, got %s", fp)
-	}
-}
-
-func TestAlertLabelAndAnnotation(t *testing.T) {
-	label := AlertLabel{Name: "severity", Value: "critical"}
-	if label.Name != "severity" || label.Value != "critical" {
-		t.Errorf("AlertLabel fields not set correctly")
-	}
-	annotation := AlertAnnotation{Name: "description", Value: "something happened"}
-	if annotation.Name != "description" || annotation.Value != "something happened" {
-		t.Errorf("AlertAnnotation fields not set correctly")
-	}
-}
+// TODO: some more tests with database interactions
