7972958432acf482c0cb5035b2ac0ecca3b9d32b
Frank <frank@enpace.ch>
2025-08-28 16:51:28 +0200

    fix bugs and create firstseen

--- internal/integrations/db/imagedbsink.go
+++ internal/integrations/db/imagedbsink.go
@@ -88,6 +88,14 @@ func (is *ImageDbSink) process() {
 				continue
 			}
 			logger.Info().Msg("Updated image metadata in database")
+			for _, finding := range pharosScanResult.Image.Findings {
+				tx := is.DatabaseContext.DB.Save(&finding)
+				if tx.Error != nil {
+					logger.Error().Err(tx.Error).Msg("Failed to update finding in database")
+					continue
+				}
+			}
+			logger.Info().Msg("Updated findings in database")
 		}
 		logger.Info().Msg("Image saved successfully")
 	}
